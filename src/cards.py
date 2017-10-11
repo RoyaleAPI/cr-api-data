83a7350be96f3b45635ba82aa8ee26cc2577994e
SML <smlbiobot@gmail.com>
2017-10-12 05:57:04 +0800

    generate cards json

--- src/cards.py
+++ src/cards.py
@@ -24,17 +24,29 @@ class App:
 
         self.cards = {}
 
-    def texts(self, tid, lang):
+    def text(self, tid, lang):
         """Return field by TID and Language
 
-        quests_hint = self.texts('TID_HINT_QUESTS', 'EN')
+        quests_hint = self.text('TID_HINT_QUESTS', 'EN')
         """
         csv_path = os.path.join(self.config.csv.base, self.config.csv.path.texts)
         with open(csv_path) as f:
             texts_reader = csv.DictReader(f)
             for row in texts_reader:
                 if row[' '] == tid:
-                    return row[lang]
+                    s = row[lang]
+                    return s.replace('\q', '\"')
+        return None
+
+    def arena_id(self, key):
+        """Return arena integer id by arena key
+        """
+        csv_path = os.path.join(self.config.csv.base, self.config.csv.path.arenas)
+        with open(csv_path) as f:
+            texts_reader = csv.DictReader(f)
+            for row in texts_reader:
+                if row['Name'] == key:
+                    return int(row['Arena'])
         return None
 
     def run(self):
@@ -48,25 +60,37 @@ class App:
 
         def card_type(type):
             """make card dicts by type."""
-            if type == 'buildings':
-                path = self.config.csv.path.spells_buildings
-            elif type == 'characters':
-                path = self.config.csv.path.spells_characters
-            elif type == 'other':
-                path = self.config.csv.path.spells_other
+            spells = {
+                'buildings': {
+                    'path': self.config.csv.path.spells_buildings,
+                    'type': 'Buildings'
+                },
+                'characters': {
+                    'path': self.config.csv.path.spells_characters,
+                    'type': 'Troops'
+                },
+                'other': {
+                    'path': self.config.csv.path.spells_other,
+                    'type': 'Spells'
+                },
+            }
 
-            csv_path = os.path.join(self.config.csv.base, path)
+            csv_path = os.path.join(self.config.csv.base, spells[type]['path'])
             with open(csv_path) as f:
                 reader = csv.DictReader(f)
                 for i, row in enumerate(reader):
                     if i > 0:
+                        name = self.text(row['TID'], 'EN')
+                        ccs = camelcase_split(name)
+                        key = '-'.join(s.lower() for s in ccs)
                         card = {
-                            'name': row['Name'],
-                            'elixir': row['ManaCost'],
-                            'type': type,
+                            'key': key,
+                            'name': name,
+                            'elixir': int(row['ManaCost']),
+                            'type': spells[type]['type'],
                             'rarity': row['Rarity'],
-                            'arena': row['UnlockArena'],
-                            'description': self.texts(row['TID_INFO'], 'EN')
+                            'arena': self.arena_id(row['UnlockArena']),
+                            'description': self.text(row['TID_INFO'], 'EN')
                         }
                         cards.append(card)
                     else:
