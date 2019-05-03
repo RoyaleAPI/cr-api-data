66970e282480cdd0fc0c2bee7ea17c18f3d59272
smlbiobot <smlbiobot@gmail.com>
2019-05-03 21:53:47 +0800

    Update data with latest csv.

--- run.py
+++ run.py
@@ -15,18 +15,18 @@ if __name__ == '__main__':
         config = Box(yaml.safe_load(f))
 
     to_run = [
-        # AllianceBadges,
-        # Cards,
-        # Rarities,
-        # ChestOrder,
-        # ClanChest,
-        # GameModes,
-        # Regions,
+        AllianceBadges,
+        Cards,
+        Rarities,
+        ChestOrder,
+        ClanChest,
+        GameModes,
+        Regions,
         Arenas,
-        # TreasureChests,
-        # CardStats,
-        # Tournaments,
-        # Challenges
+        TreasureChests,
+        CardStats,
+        Tournaments,
+        Challenges
     ]
 
     for cls in to_run:
