68cbe6901a358d55591bf1a51107dce31193d45d
smlbiobot <smlbiobot@gmail.com>
2020-09-08 00:17:34 +0800

    Balance changes

---
+++ cr/emotes.py
@@ -0,0 +1,28 @@
+"""
+Game modes
+"""
+
+from csv2json import read_csv
+from .base import BaseGen
+
+
+class Emotes(BaseGen):
+    def __init__(self, config):
+        super().__init__(config, id="emotes", null_int=True)
+
+    def run(self):
+        data_list = read_csv(self.csv_path)
+
+        out = []
+        for index, row in enumerate(data_list):
+            if row.get('name') is not None:
+                hi = row.get('index_hi', '')
+                lo = row.get('index_lo', '')
+                sc = row.get('sc_file', '')[3:] # truncate path sc/
+                row.update(
+                    key=f"emote_{hi}_{lo}",
+                    file_key=f"{sc}:{hi}:{lo}"
+                )
+                out.append(row)
+
+        self.save_json(out)
